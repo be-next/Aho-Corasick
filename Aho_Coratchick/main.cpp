606014b805c325aeecbe1c9b6050c81fece36567
Jérôme Ramette <jerome.ramette@gmail.com>
2020-12-13 18:50:06 +0100

    Initial Commit

---
+++ Aho_Coratchick/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Aho_Coratchick
+//
+//  Created by Jérôme Ramette on 13/12/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
